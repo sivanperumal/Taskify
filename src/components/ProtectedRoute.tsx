9757407a54703f492c856c248cb9d42850ade031
sivanperumal <s.perumal2013@gmail.com>
2025-05-26 16:16:32 +0530

    Site Under Construction

--- src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router";
-import { useUser } from "../redux/slices/user.slice";
-
-const ProtectedRoute: React.FC = () => {
-  const { isAuthenticated } = useUser();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-};
-export default ProtectedRoute;
